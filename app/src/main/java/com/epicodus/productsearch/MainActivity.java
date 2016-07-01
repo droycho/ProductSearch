e27071db8a3633689623f9ebff168c3ad3de147d
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-01 11:21:15 -0700

    added user input.

--- app/src/main/java/com/epicodus/productsearch/MainActivity.java
+++ app/src/main/java/com/epicodus/productsearch/MainActivity.java
@@ -3,13 +3,11 @@ package com.epicodus.productsearch;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindProductButton;
     private EditText mProductEditText;
 
@@ -23,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     String product = mProductEditText.getText().toString();
-                    Log.d(TAG, product);
                     Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+                    intent.putExtra("product", product);
                     startActivity(intent);
                 }
             });
--- app/src/main/java/com/epicodus/productsearch/ProductActivity.java
+++ app/src/main/java/com/epicodus/productsearch/ProductActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.productsearch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ProductActivity extends AppCompatActivity {
+    private TextView mProductTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        mProductTextView = (TextView) findViewById(R.id.productTextView);
+        Intent intent = getIntent();
+        String product = intent.getStringExtra("product");
+        mProductTextView.setText("Here are all the stores that carry " + product);
     }
 }
