b0a0c5e54ea2d06d170784af9f5aac94c47285e0
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-01 10:59:27 -0700

    add ProductActivity and replaced toast with intent.

--- app/src/main/java/com/epicodus/productsearch/MainActivity.java
+++ app/src/main/java/com/epicodus/productsearch/MainActivity.java
@@ -1,10 +1,10 @@
 package com.epicodus.productsearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindProductButton;
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
             mFindProductButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+                    startActivity(intent);
                 }
             });
     }
