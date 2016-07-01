f7f95a97c58ddebcfd1a7bb350c9ceba9cf5d24a
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-07-01 10:54:23 -0700

    add toast to button to make sure it works, works.

--- app/src/main/java/com/epicodus/productsearch/MainActivity.java
+++ app/src/main/java/com/epicodus/productsearch/MainActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.productsearch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindProductButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindProductButton = (Button) findViewById(R.id.findProductButton);
+            mFindProductButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
+
